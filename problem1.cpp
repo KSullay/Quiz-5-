472abe8b03612a889777e6eee9d4a17a4a15367e
KSullay <160401557+KSullay@users.noreply.github.com>
2025-12-02 10:29:59 -0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count[3] = {0, 0, 0};
+        for (int num : nums) {
+            count[num]++;
+        }
+        int index = 0;
+        for (int i = 0; i < count[0]; i++) {
+            nums[index++] = 0;
+        }
+        for (int i = 0; i < count[1]; i++) {
+            nums[index++] = 1;
+        }
+        for (int i = 0; i < count[2]; i++) {
+            nums[index++] = 2;
+        }
+    }
+};
