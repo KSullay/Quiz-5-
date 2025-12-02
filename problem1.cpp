1cead47f64b71238baa3f78ffcca248766bdd96b
KSullay <160401557+KSullay@users.noreply.github.com>
2025-12-02 10:46:14 -0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,22 +1,30 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortArrays(vector<int>& nums) {
         int count[3] = {0, 0, 0};
+
         for (int num : nums) {
             count[num]++;
         }
+
         int index = 0;
-        for (int i = 0; i < count[0]; i++) {
-            nums[index++] = 0;
-        }
-        for (int i = 0; i < count[1]; i++) {
-            nums[index++] = 1;
-        }
-        for (int i = 0; i < count[2]; i++) {
-            nums[index++] = 2;
-        }
+        for (int i = 0; i < count[0]; i++) nums[index++] = 0;
+        for (int i = 0; i < count[1]; i++) nums[index++] = 1;
+        for (int i = 0; i < count[2]; i++) nums[index++] = 2;
     }
 };
+
+int main() {
+    Solution s;
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    s.sortArrays(nums);
+    for (int n : nums) {
+        cout << n << " ";
+    }
+    cout << endl;
+    return 0;
+}
